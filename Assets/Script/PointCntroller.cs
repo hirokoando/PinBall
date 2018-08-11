574a911cd7edc8aad73ed469f425d7b284f9d655
hirokoando <01074ahinrodoko4@gmail.com>
2018-08-11 18:43:06 +0900

    Lesson5 3Dゲーム１　PinBall
    PinBall作成と課題、発展課題

---
+++ Assets/Script/PointCntroller.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointCntroller : MonoBehaviour {
+
+    private GameObject pointText;
+    private int sumPoint;
+    private int point;
+    private int judge;
+    
+
+    // Use this for initialization
+    void Start () {
+
+        this.pointText = GameObject.Find("PointText");
+        sumPoint = 0;
+        judge = 0;
+
+        if (tag == "SmallStarTag")
+        {
+            this.point = 0;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.point = 5;
+        }
+        else if (tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            this.point = 1;
+        }
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       if (judge == 1)
+       {
+            sumPoint += point;
+            judge = 0;
+       }
+
+        this.pointText.GetComponent<Text>().text = "Point:" + sumPoint;
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        judge = 1;
+
+    }
+
+}
---
+++ Assets/Script/ScoreController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+    private GameObject ScoreText;
+    private int sumScore;
+    
+    // Use this for initialization
+    void Start () {
+        this.ScoreText = GameObject.Find("ScoreText");
+        sumScore = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+        this.ScoreText.GetComponent<Text>().text = "Score:" + sumScore;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+       
+        if (other.gameObject.tag == "LargeStarTag")
+        {
+            sumScore += 5;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag" || other.gameObject.tag == "LargeCloudTag")
+        {
+            sumScore += 1;
+        }
+
+    }
+
+}
